8da76dcae1e4641bc23e3667fe17e08f253af24e
aek20 <azozek1420@gmail.com>
2023-11-18 07:54:04 -1000

    edit hotels and rooms and room details page

--- tuwaiq_sec/Controllers/RoomsDetailsController.cs
+++ tuwaiq_sec/Controllers/RoomsDetailsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using tuwaiq_sec.Data;
+using tuwaiq_sec.Models;
 
 namespace tuwaiq_sec.Controllers
 {
@@ -11,17 +12,43 @@ namespace tuwaiq_sec.Controllers
         {
             _db = db;
         }
+     
         [HttpGet]
         [Route("/RoomsDetails/{roomId}")]
-        public IActionResult Index(int id)
+        public IActionResult Index(int roomId)
         {
-            var roomDetail = _db.RoomDetail.Find(1);
+            var roomDetail = _db.RoomDetail.FirstOrDefault(rd => rd.RoomId == roomId);
             if (roomDetail == null)
             {
                 return NotFound();
             }
 
+
             return View(roomDetail);
         }
+        [HttpGet]
+        [Route("/RoomDetails/Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(RoomDetail room)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save the new hotel to the database
+                _db.RoomDetail.Add(room);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", new { roomId = room.RoomId });
+            }
+
+            return View(room);
+        }
+
+
+
     }
 }
